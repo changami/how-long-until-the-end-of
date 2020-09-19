1e47f908c6fd5b0b248b29c7289759383743f3d3
chan_gami <babyryo@msn.com>
2020-09-19 04:59:20 -0700

    :arrow_up: upgrade dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,7 @@ const production = !process.env.ROLLUP_WATCH;
 export default {
   input: 'src/main.js',
   output: {
-    sourcemap: true,
+    sourcemap: !production,
     format: 'iife',
     name: 'app',
     file: 'public/build/bundle.js',
@@ -21,7 +21,7 @@ export default {
       // we'll extract any component CSS out into
       // a separate file — better for performance
       css: css => {
-        css.write('public/build/bundle.css');
+        css.write('bundle.css', !production);
       },
     }),
 
